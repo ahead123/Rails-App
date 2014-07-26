9174590cf376f0090721e411fc7e7d66a43b4ac4
Arthur Head <arthurallenhead@yahoo.com>
2014-07-26 02:26:04 -0400

    todo_lists scaffold generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = 'da72f270c0a08b0d8967c8490f7355e9a237376797e85ce1c9b2ab23874cb2401f14756063b86d645eb02772c25b994e9ca2ff3bf1221b75f53bceffc9eaeba8'
