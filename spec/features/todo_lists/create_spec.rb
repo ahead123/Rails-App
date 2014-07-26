5b3cdff64398c352c6636ce08888746cf21822fe
Arthur Head <arthurallenhead@yahoo.com>
2014-07-26 03:50:00 -0400

    Add length and presence validation to todo lists title

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 describe "creating todo lists" do 
 	it "redirects to the todo list index page on success" do
@@ -19,6 +19,24 @@ describe "creating todo lists" do
 	expect(page).to have_content("New todo_list")
 
 	fill_in "Title", with: ""
+	fill_in "Description", with: "This is what I'm doing today."
+	click_button "Create Todo list"
+	
+	expect(page).to have_content("error")
+	expect(TodoList.count).to eq(0)
+
+	visit "/todo_lists"
+	expect(page).to_not have_content("This is what I'm doing today.")
+
+end
+
+it "displays an error when the todo list has a title less than 3 characters" do
+		expect(TodoList.count).to eq(0)
+		visit "/todo_lists"
+	click_link "New Todo list"
+	expect(page).to have_content("New todo_list")
+
+	fill_in "Title", with: "Hi"
 	fill_in "Description", with: "This is what I'm doing today."
 	click_button "Create Todo list"
 	
